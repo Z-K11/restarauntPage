c034945f3326ab3da55777901b30829eeec02b21
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-07 11:57:10 +0500

    about page finished

--- src/aboutScript.js
+++ src/aboutScript.js
@@ -4,4 +4,14 @@ export function aboutSection(logo)
     const contentDiv = document.querySelector('#content');
     contentDiv.appendChild(aboutHeading);
     aboutHeading.textContent='About Me';
+    contentDiv.classList.add('aboutContent');
+    aboutHeading.classList.add('aboutHeading');
+    const aboutDescription = document.createElement('p');
+    contentDiv.appendChild(aboutDescription);
+    aboutDescription.textContent='Greetings, I am a Junior full stack developer currently studying under the supervision of the awesome team at TheOdinProject. I love programming everything myself and hate vibeCoding. IF you have any cool projects of your own please share them with me. THANKS!';
+    aboutDescription.classList.add('aboutDescription');
+    const contact = document.createElement('p');
+    contentDiv.appendChild(contact);
+    contact.textContent='feel free to contact me via : zulqarnainbazidkhail@gmail.com';
+    contact.classList.add('contact');
 }
